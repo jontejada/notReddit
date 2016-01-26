16c9b86aae966408601351a6f784c9c65f577306
Jon Tejada <jon@jontejada.com>
2016-01-25 19:00:54 -0800

    lots of styling

--- js/reddit.js
+++ js/reddit.js
@@ -1,6 +1,5 @@
 var redditApp = angular.module('redditApp', []);
 
 redditApp.controller('bodyController', function($scope) {
-	$scope.test = 24;
-	$scope.names = ['Harry','Harry', 'Ron', 'Hermione', 'Sirius', 'Hedwig', 'Tonks'];
-})
+	
+});
