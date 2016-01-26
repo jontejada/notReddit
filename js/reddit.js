773bb74ead8ea52640329bcf32d9a4dfbc242258
Jon Tejada <jon@jontejada.com>
2016-01-25 23:08:40 -0800

    working angular with background directive

--- js/reddit.js
+++ js/reddit.js
@@ -1,5 +1,153 @@
-var redditApp = angular.module('redditApp', []);
+var app = angular.module('redditApp', []);
 
-redditApp.controller('bodyController', function($scope) {
-	
+app.controller('bodyController', function($scope) {
+	$scope.newComment = {};
+	$scope.newPost = {};
+	$scope.posts = {};
+	$scope.posts.search = "";
+	$scope.posts.newPostViz = false;
+	$scope.posts.filters = ['votes','date','title'];
+	$scope.posts.filter = $scope.posts.filters[0];
+	$scope.posts.order = '-votes';
+	$scope.posts.all = [
+		{
+			title: "Point Reyes Beaches",
+			author: "Linos Godeliva",
+			image:"http://www.jontejada.com/assets/DSC_0530.jpg",
+			description: "Master cleanse venmo blog eiusmod nisi. Schlitz placeat iPhone food truck blog bitters street art. Delectus laborum gluten-free chicharrones.",
+			votes: 12,
+			date: moment().subtract(1,'days').subtract(1,'hours').calendar(),
+			comments: {
+				newViz: false,
+				allViz: false,
+				all: [
+					{
+						author: "Jon",
+						text: "cool!"
+					}
+				]
+			}
+		},
+		{
+			title: "Fence & Bell",
+			author: "Vira Valeria",
+			image:"http://www.jontejada.com/assets/DSC_0774.jpg",
+			description: "Microdosing tote bag tumblr etsy YOLO, pickled hammock ethical adipisicing selvage excepteur health goth narwhal.",
+			votes: 6,
+			date: moment().subtract(3,'days').subtract(2,'hours').calendar(),
+			comments: {
+				newViz: false,
+				allViz: false,
+				all: [
+					{
+						author: "Jon",
+						text: "cool!"
+					}
+				]
+			}
+		},
+		{
+			title: "Point Reyes Sunset",
+			author: "Samanta Samuil",
+			image:"http://www.jontejada.com/assets/DSC_0523.jpg",
+			description: "Chia occaecat church-key franzen sustainable disrupt scenester. Yuccie slow-carb listicle drinking vinegar tote bag.",
+			votes: 3,
+			date: moment().subtract(20,'days').subtract(4,'hours').calendar(),
+			comments: {
+				newViz: false,
+				allViz: false,
+				all: [
+					{
+						author: "Jon",
+						text: "cool!"
+					}
+				]
+			}
+		},
+		{
+			title: "Santa Maria Beach",
+			author: "Claudio Jet",
+			image:"http://www.jontejada.com/assets/DSC_0507.jpg",
+			description: "Umami ullamco try-hard pickled, pinterest elit four loko dolore everyday carry. Aliqua ennui try-hard exercitation, brunch flexitarian shabby chic offal wayfarers occaecat asymmetrical.",
+			votes: 24,
+			date: moment().subtract(1,'days').subtract(2,'hours').calendar(),
+			comments: {
+				newViz: false,
+				allViz: false,
+				all: [
+					{
+						author: "Jon",
+						text: "cool!"
+					}
+				]
+			}
+		},
+		{
+			title: "Sea Ranch Lodge",
+			author: "Sybilla Vyacheslav",
+			image:"http://www.jontejada.com/assets/DSC_0778.jpg",
+			description: "Labore et ullamco kombucha. 90's ex anim, retro tilde aliquip vice sustainable duis mixtape microdosing ugh. Hashtag labore four dollar toast raw denim.",
+			votes: 2,
+			date: moment().subtract(0,'days').subtract(8,'hours').calendar(),
+			comments: {
+				newViz: false,
+				allViz: false,
+				all: [
+					{
+						author: "Jon",
+						text: "cool!"
+					}
+				]
+			}
+		},
+		{
+			title: "Old Barn",
+			author: "Lea Gul",
+			image:"http://www.jontejada.com/assets/DSC_0796.jpg",
+			description: "Aliqua church-key drinking vinegar synth. PBR&B kogi pork belly 3 wolf moon, wolf ut in blog artisan exercitation culpa cliche.",
+			votes: -12,
+			date: moment().subtract(15,'days').subtract(1,'hours').calendar(),
+			comments: {
+				newViz: false,
+				allViz: false,
+				all: [
+					{
+						author: "Jon",
+						text: "cool!"
+					}
+				]
+			}
+		},
+		{
+			title: "Stormy Sea",
+			author: "Anouska Iantha",
+			image:"http://www.jontejada.com/assets/DSC_0782.jpg",
+			description: "PBR&B pop-up food truck austin elit accusamus VHS pinterest, sapiente banh mi cred pug trust fund dreamcatcher skateboard. Actually cronut.",
+			votes: 42,
+			date: moment().subtract(3,'days').subtract(1,'hours').calendar(),
+			comments: {
+				newViz: false,
+				allViz: false,
+				all: [
+					{
+						author: "Jon",
+						text: "cool!"
+					}
+				]
+			}
+		}
+	];
 });
+
+
+app.directive('backImg', function(){
+    return function(scope, element, attrs){
+        attrs.$observe('backImg', function(value) {
+            element.css({
+                'background-image': 'url(' + value +')',
+                'background-size' : 'cover'
+            });
+        });
+    };
+});
+
